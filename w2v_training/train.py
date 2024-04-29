322b570f38bc38c65833795af240eeb19308d54c
Gustavo <gustavo.reis2@discente.ufg.br>
2024-04-29 15:18:08 -0300

    w2v training added

---
+++ w2v_training/train.py
@@ -0,0 +1,156 @@
+import os
+import argparse
+
+import pandas as pd
+from omegaconf import OmegaConf
+from audiomentations import Compose
+from sklearn.model_selection import train_test_split
+from transformers.trainer_utils import get_last_checkpoint
+from transformers import (
+                            AutoModelForAudioClassification,
+                            EarlyStoppingCallback,
+                            AutoFeatureExtractor,
+                            TrainingArguments,
+                            Trainer
+)
+
+from utils.utils import (
+    DataColletorTrain, compute_metrics, preprocess_metadata, get_label_id, map_data_augmentation
+)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--config_path',
+        default='config/default.yaml',
+        type=str,
+        help="YAML file with configurations"
+    )
+    parser.add_argument(
+        '--continue_train',
+        default=False,
+        action='store_true',
+        help='If True, continues training using the checkpoint_path parameter'
+    )
+    args = parser.parse_args()
+
+    cfg = OmegaConf.load(args.config_path)
+
+    if os.path.isdir(cfg.train.model_checkpoint):
+        last_checkpoint = get_last_checkpoint(cfg.train.model_checkpoint)
+        print("> Resuming Train with checkpoint: ", last_checkpoint)
+    else:
+        last_checkpoint = None
+
+    if cfg.data.audio_augmentator and cfg.data.apply_augmentation:
+        audio_augmentator = Compose([map_data_augmentation(aug_config) for aug_config in cfg.data.audio_augmentator])
+    else:
+        audio_augmentator = None
+
+    metadatas_train = [
+        '/home/gustavo/Projects/PAV/DEEPFAKE-COMPTETITION-PAV/audios/metadata/treino.csv'
+    ]
+    metadatas_val = [
+        '/home/gustavo/Projects/PAV/DEEPFAKE-COMPTETITION-PAV/audios/metadata/validacao.csv'
+    ]
+    modelo_to_save = cfg.model_checkpoint
+    logging = cfg.logging
+    for metadata_train, metadata_validation in zip(metadatas_train,metadatas_val):
+        train_df = pd.read_csv(metadata_train)
+        print("Training with metadata: ", metadata_train)
+        log_name = f'{logging}-{modelo_to_save}'
+        weights_output_path = cfg.weights_output_path
+        val_df = pd.read_csv(metadata_validation)
+
+        train_dataset = preprocess_metadata(base_dir='', cfg=cfg, df=train_df)
+        val_dataset = preprocess_metadata(base_dir='', cfg=cfg, df=val_df)
+
+        label2id = {
+            'real':0,
+            'fake':1
+        }
+
+        id2label = {
+            0:"real",
+            1:'fake'
+        }
+
+        feature_extractor = AutoFeatureExtractor.from_pretrained(cfg.train.model_checkpoint)
+        model = AutoModelForAudioClassification.from_pretrained(
+            pretrained_model_name_or_path=last_checkpoint if last_checkpoint else cfg.train.model_checkpoint,
+            num_labels=len(label2id.keys()),
+            label2id=label2id,
+            id2label=id2label,
+        )
+
+        data_collator = DataColletorTrain(
+            feature_extractor,
+            apply_augmentation=cfg.data.apply_augmentation,
+            audio_augmentator=audio_augmentator,
+            sampling_rate=cfg.data.target_sampling_rate,
+            padding=cfg.data.pad_audios,
+            apply_dbfs_norm=cfg.data.apply_dbfs_norm,
+            target_dbfs=cfg.data.target_dbfs,
+            label2id=label2id,
+            max_audio_len=cfg.data.max_audio_len
+        )
+
+        train_args = TrainingArguments(
+            output_dir=weights_output_path,
+            run_name=log_name,
+            report_to="all",
+            save_strategy="epoch",
+            evaluation_strategy="epoch",
+            learning_rate=cfg.train.learning_rate,
+            dataloader_num_workers=cfg.train.num_workers,
+            per_device_train_batch_size=cfg.train.batch_size,
+            per_device_eval_batch_size=cfg.train.batch_size,
+            gradient_accumulation_steps=cfg.train.gradient_accumulation_steps,
+            save_total_limit=cfg.train.save_total_limit,
+            metric_for_best_model=cfg.train.metric,
+            logging_steps=cfg.train.logging_steps,
+            warmup_ratio=cfg.train.warmup_ratio,
+            num_train_epochs=cfg.train.epochs,
+            load_best_model_at_end=True,
+            logging_first_step=True,
+            greater_is_better=True,
+            seed=cfg.train.seed,
+            fp16=True
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=train_args,
+            data_collator=data_collator,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            tokenizer=feature_extractor,
+            compute_metrics=compute_metrics
+        )
+
+        if cfg.train.use_early_stop:
+            trainer.add_callback(EarlyStoppingCallback(early_stopping_patience=cfg.train.early_stop_epochs))
+
+        print("> Starting Training")
+        train_result = trainer.train(resume_from_checkpoint=last_checkpoint if args.continue_train else None)
+        # Save best model
+        trainer.save_model(f'models/best_model_{modelo_to_save}')
+
+        # Save train results
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(train_dataset)
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        # Save eval results
+        print("--- Evaluate ---")
+        metrics = trainer.evaluate()
+        metrics["eval_samples"] = len(val_dataset)
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+if __name__ == '__main__':
+    main()
---
+++ w2v_training/utils/utils.py
@@ -0,0 +1,291 @@
+import os
+import operator
+import functools
+import traceback
+from typing import Dict, List, Optional, Union
+
+import torch
+import tqdm
+import torch.nn.functional as F
+import torchaudio
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pydub import AudioSegment
+from omegaconf import DictConfig
+import matplotlib.pyplot as plt
+from datasets import Dataset, load_metric
+from transformers import Wav2Vec2Processor
+from sklearn.metrics import confusion_matrix
+from audiomentations import AddGaussianNoise, PitchShift
+
+metric = load_metric("f1")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def compute_metrics(eval_pred):
+    """Computes metric on a batch of predictions"""
+    predictions = np.argmax(eval_pred.predictions, axis=1)
+    return metric.compute(predictions=predictions, references=eval_pred.label_ids, average="macro")
+
+
+def map_path(batch, cfg):
+    """Maps the real path to the audio files"""
+    path = os.path.join(batch[cfg.metadata.audio_path_column].lstrip('/'))
+    batch['input_values'] = path
+    return batch
+
+
+def preprocess_metadata(base_dir: str , cfg: DictConfig, df: pd.DataFrame):
+    """Maps the real path to the audio files"""
+    df.reset_index(drop=True, inplace=True)
+
+    df_dataset = Dataset.from_pandas(df)
+    df_dataset = df_dataset.map(
+        map_path,
+        fn_kwargs={"base_dir": base_dir, "cfg": cfg},
+        num_proc=cfg.train.num_workers
+    )
+
+    return df_dataset
+
+
+def get_label_id(dataset: Dataset, label_column: str):
+    """Gets the labels IDs"""
+    label2id, id2label = dict(), dict()
+
+    labels = dataset.unique(label_column)
+    labels.sort()
+
+    num_labels = len(id2label)
+
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+
+    return label2id, id2label, num_labels
+
+
+def predict(test_dataloader, model, cfg):
+    model.to(device)
+    model.eval()
+    preds = []
+    paths = []
+    with torch.no_grad():
+        for batch in tqdm.tqdm(test_dataloader):
+            input_values, attention_mask = batch['input_values'].to(device), batch['attention_mask'].to(device)
+
+            logits = model(input_values, attention_mask=attention_mask).logits
+
+            scores = F.softmax(logits, dim=-1)
+            pred = torch.argmax(scores, dim=1).cpu().detach().numpy()
+
+            preds.extend(pred)
+
+    return preds
+
+
+def map_data_augmentation(aug_config):
+    # Adapted from: https://github.com/Edresson/Wav2Vec-Wrapper
+    aug_name = aug_config.name
+    del aug_config.name
+    if aug_name == 'gaussian':
+        return AddGaussianNoise(**aug_config)
+    elif aug_name == 'pitch_shift':
+        return PitchShift(**aug_config)
+    else:
+        raise ValueError("The data augmentation '" + aug_name + "' doesn't exist !!")
+
+class DataColletorTrain:
+    # Adapted from https://github.com/Edresson/Wav2Vec-Wrapper
+    def __init__(
+        self,
+        processor: Wav2Vec2Processor,
+        apply_augmentation: bool = False,
+        audio_augmentator: List[Dict] =  None,
+        sampling_rate: int = 16000,
+        padding: Union[bool, str] = True,
+        max_length: Optional[int] = None,
+        pad_to_multiple_of: Optional[int] = None,
+        apply_dbfs_norm: Union[bool, str] = False,
+        target_dbfs: int = 0.0,
+        label2id: Dict = None,
+        max_audio_len: int = 20
+    ):
+
+        self.processor = processor
+        self.sampling_rate = sampling_rate
+
+        self.padding = padding
+        self.max_length = max_length
+        self.pad_to_multiple_of = pad_to_multiple_of
+
+        self.apply_dbfs_norm = apply_dbfs_norm
+        self.target_dbfs = target_dbfs
+
+        self.apply_augmentation = apply_augmentation
+        self.audio_augmentator = audio_augmentator
+
+        self.label2id = label2id
+
+        self.max_audio_len = max_audio_len
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lenghts and need
+        # different padding methods
+        input_features = []
+        label_features = []
+        for feature in features:
+            try:
+                # Gain Normalization
+                if self.apply_dbfs_norm:
+                    # Audio is loaded in a byte array
+                    sound = AudioSegment.from_file(feature["input_values"], format="wav")
+                    sound = sound.set_channels(1)
+                    change_in_dBFS = self.target_dbfs - sound.dBFS
+                    # Apply normalization
+                    normalized_sound = sound.apply_gain(change_in_dBFS)
+                    # Convert array of bytes back to array of samples in the range [-1, 1]
+                    # This enables to work wih the audio without saving on disk
+                    norm_audio_samples = np.array(normalized_sound.get_array_of_samples()).astype(np.float32, order='C') / 32768.0
+
+                    if sound.channels < 2:
+                        norm_audio_samples = np.expand_dims(norm_audio_samples, axis=0)
+
+                    # Expand one dimension and convert to torch tensor to have the save output shape and type as torchaudio.load
+                    speech_array = torch.from_numpy(norm_audio_samples)
+                    sampling_rate = sound.frame_rate
+
+                # Load wav
+                else:
+                    speech_array, sampling_rate = torchaudio.load(feature["input_values"])
+
+                # Transform to Mono
+                speech_array = torch.mean(speech_array, dim=0, keepdim=True)
+
+                if sampling_rate != self.sampling_rate:
+                    transform = torchaudio.transforms.Resample(sampling_rate, self.sampling_rate)
+                    speech_array = transform(speech_array)
+                    sampling_rate = self.sampling_rate
+
+                effective_size_len = sampling_rate * self.max_audio_len
+
+                if speech_array.shape[-1] > effective_size_len:
+                    speech_array = speech_array[:, :effective_size_len]
+
+                speech_array = speech_array.squeeze().numpy()
+                input_tensor = self.processor(speech_array, sampling_rate=sampling_rate).input_values
+                input_tensor = np.squeeze(input_tensor)
+
+                if self.audio_augmentator is not None and self.apply_augmentation:
+                    input_tensor = self.audio_augmentator(input_tensor, sample_rate=self.sampling_rate).tolist()
+
+                input_features.append({"input_values": input_tensor})
+                label_features.append(int(self.label2id[feature["label"]]))
+            except Exception:
+                print("Error during load of audio:", feature["input_values"])
+                continue
+
+        batch = self.processor.pad(
+            input_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        batch["labels"] = torch.tensor(label_features)
+
+        return batch
+
+
+class DataColletorTest:
+    def __init__(
+        self,
+        processor: Wav2Vec2Processor,
+        sampling_rate: int = 16000,
+        padding: Union[bool, str] = True,
+        max_length: Optional[int] = None,
+        pad_to_multiple_of: Optional[int] = None,
+        label2id: Dict = None,
+        max_audio_len: int = 20
+    ):
+
+        self.processor = processor
+        self.sampling_rate = sampling_rate
+
+        self.padding = padding
+        self.max_length = max_length
+        self.pad_to_multiple_of = pad_to_multiple_of
+
+        self.label2id = label2id
+
+        self.max_audio_len = max_audio_len
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # split inputs and labels since they have to be of different lenghts and need
+        # different padding methods
+        input_features = []
+        label_features = []
+        for feature in features:
+            # Load wav
+            speech_array, sampling_rate = torchaudio.load(feature["input_values"])
+
+            # Transform to Mono
+            speech_array = torch.mean(speech_array, dim=0, keepdim=True)
+
+            if sampling_rate != self.sampling_rate:
+                transform = torchaudio.transforms.Resample(sampling_rate, self.sampling_rate)
+                speech_array = transform(speech_array)
+                sampling_rate = self.sampling_rate
+
+            effective_size_len = sampling_rate * self.max_audio_len
+
+            if speech_array.shape[-1] > effective_size_len:
+                speech_array = speech_array[:, :effective_size_len]
+
+            speech_array = speech_array.squeeze().numpy()
+            input_tensor = self.processor(speech_array, sampling_rate=sampling_rate).input_values
+            input_tensor = np.squeeze(input_tensor)
+
+            input_features.append({"input_values": input_tensor})
+            label_features.append(int(self.label2id[feature["label"]]))
+
+        batch = self.processor.pad(
+            input_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        batch["labels"] = torch.tensor(label_features)
+
+        return batch
+
+def save_conf_matrix(
+    targets: List[int],
+    preds: List[int],
+    classes: List[str],
+    output_path: str
+) -> None:
+    """
+    Saves a confusion matrix given the true labels and the predicted outputs.
+    """
+    cm = confusion_matrix(
+        y_true=targets,
+        y_pred=preds
+    )
+
+    df_cm = pd.DataFrame(
+        cm,
+        index=classes,
+        columns=classes
+    )
+
+    plt.figure(figsize=(24,12))
+    plot = sns.heatmap(df_cm, annot=True,  fmt='g')
+    figure1 = plot.get_figure()
+    plot.set_ylabel('True Label')
+    plot.set_xlabel('Predicted Label')
+    plt.tight_layout()
+    figure1.savefig(output_path, format='png')
